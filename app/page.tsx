d2427501fbe6ac1edc74bfd660414c053caa9a99
prashant48653c <acharyaprashant227@gmail.com>
2024-11-10 13:05:51 +0545

    Created hero section

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/landing/hero";
+import Image from "next/image";
+
+ 
+export default function Home() {
+  return (
+   <main className="w-full">
+   <Hero/>
+ </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        lightblue: '#f0f8ff',   // Alice Blue
+        lightgoldenrod: '#fafad2', // Light Goldenrod Yellow
+        lightcyan: '#e0ffff',    // Light Cyan
+        whitesmoke: '#f5f5f5',
+      },
+      borderColor: {
+        lightblue: '#ebac72',         // Alice Blue Border
+        lightgoldenrod: '#ddc1d2',    // Light Goldenrod Yellow Border
+        lightcyan: '#c2e8ff',         // Light Cyan Border
+        whitesmoke: '#dcf9de',        // White Smoke Border
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
